6c3232aff34587e45fff58bc9211f66a2026ccdf
Yagiz P <yagizp@Yagizs-MBP.home>
2024-09-03 13:26:20 +0200

    Work in progress

--- src/app/views/details-page/details-page.component.ts
+++ src/app/views/details-page/details-page.component.ts
@@ -1,40 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PokemonService } from '../../core/services/pokemon.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from '../../core/models/pokemon.model';
-import { catchError, map, switchMap } from 'rxjs';
+import { catchError, map, Observable, switchMap } from 'rxjs';
 
 @Component({
     selector: 'app-details-page',
     templateUrl: './details-page.component.html',
     styleUrl: './details-page.component.css'
 })
-export class DetailsPageComponent implements OnInit {
-    private pokemonId: number | undefined;
+export class DetailsPageComponent {
 
+    protected pokemon$: Observable<Pokemon | undefined> | undefined;
     protected pokemon: Pokemon | undefined;
 
     constructor(
         private pokemonService: PokemonService,
         private router: Router,
         private route: ActivatedRoute
-    ) {}
-
-    ngOnInit() {
-        this.route.params.pipe(
+    ) {
+        this.pokemon$ = this.route.params.pipe(
             map((params) => {
-                console.log("Params", params);
                 return +params["id"];
             }),
             switchMap((id: number) => this.pokemonService.getPokemonById(id)),
+            map((pokemon: Pokemon | undefined) => {
+                this.pokemon = pokemon;
+                return pokemon;
+            }),
             catchError(() => {
                 throw new Error("No pokemon found");
             })
-        ).subscribe({
-            next: (pokemon: Pokemon | undefined) => {
-                console.log("pokemon", pokemon);
-                this.pokemon = pokemon;
-            }
-        });
+        );
+    }
+
+    protected backBtnClickHandler(): void {
+        this.router.navigate(["list"]).then();
     }
 }
